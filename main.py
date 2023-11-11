099b18ade455ffccc068cf1edc19aad632891f3b
Huijeong Kim <herehuijeong@gmail.com>
2023-11-11 19:32:09 +0900

    extract chatgpt client to simple_chatgpt_client.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from openai import OpenAI
 from dotenv import load_dotenv
 import os
 import streamlit as st
 
+from simple_chatgpt_client import SimpleClient
+
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
-client = OpenAI(api_key=api_key)
+client = SimpleClient(api_key)
 
 st.title('ElasticSearch query builder')
 st.divider()
@@ -22,26 +23,10 @@ query = st.text_input("query you want in natural language:",
 result="No Result"
 
 if st.button('build'):
-    completion = client.chat.completions.create(
-        messages=[
-            {
-                "role": "system",
-                "content": "Provide proper elastic search query with provided fields. Do not explain, just a query"
-            },
-            {
-                "role": "user",
-                "content": f"with the doc fields {layout}, {query}"
-            }
-        ],
-        model="gpt-3.5-turbo"
-    )
-    
-    result = completion.choices[0].message.content
-    
+    result = client.get(layout, query)    
 
 st.write("")
 st.write("")
 st.write("")
 
 st.code(result, language="json")
-
